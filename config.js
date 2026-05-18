084b5ddc454829c50cb9d8b6d98fccd9ce2d4c0a
le5125 <132064477+le5125@users.noreply.github.com>
2026-05-18 09:52:34 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // GAS 部署完成後，把網址貼到這裡。
 // 例：https://script.google.com/macros/s/你的部署ID/exec
 window.APP_CONFIG = {
-  GAS_WEB_APP_URL: ""
+  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxugZM52HlLboXW9c3MIS-331D9t6CB5hA6cXHXKekcmQURmYfIoNyIsdKKFD1CZKK_Bw/exec"
 };
